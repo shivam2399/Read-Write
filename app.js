10d0eda68beebe0afba991062a4c510e7d5516fd
shivam2399 <dubey.shivam2399@gmail.com>
2025-07-04 22:32:08 +0530

    Extracting the input mesasge

--- app.js
+++ app.js
@@ -5,49 +5,39 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    if(url === '/') {
+    if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
+        fs.readFile('message.txt', (err, data) => {
+            const message = err ? 'No message yet.' : data.toString();
 
-        res.end(`
-            <form action="/message" method="POST">
-             <label>Name:</label>
-             <input type="text" name="username"></input>
-             <button type="submit">Add</button>
-            </form>
-        `);
-    } else {
-        if(req.url=="/message") {
-            res.setHeader('Content-Type', 'text/html');
+            res.end(`
+                <h1>${message}</h1>
+                <form action="/message" method="POST">
+                    <label>Name:</label>
+                    <input type="text" name="username" />
+                    <button type="submit">Add</button>
+                </form>
+            `);
+        });
+    } else if (url === '/message' && method === 'POST') {
+        let dataChunks = [];
+        req.on('data', (chunk) => {
+            dataChunks.push(chunk);
+        });
 
-            let dataChunks = [];
-            req.on('data', (chunk) => {
-                dataChunks.push(chunk);
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(dataChunks).toString();
+            const value = parsedBody.split('=')[1];
+
+            fs.writeFile('message.txt', value, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
             });
-
-            req.on('end', () => {
-                let combinedBuffer = Buffer.concat(dataChunks);
-                let value = combinedBuffer.toString().split('=')[1];
-
-                fs.writeFile('message.txt', value, (err) => {
-                    
-                    res.statusCode = 302; // redirect status code
-                    res.setHeader('Location', '/');
-                    res.end()
-                });
-            });
-        } else {
-            if(url == '/read') {
-                fs.readFile('message.txt', (err, data) => {
-                    console.log(data.toString());
-                    res.end(`
-                        <h1>${data.toString()}</h1>
-                    `);
-                })
-            }
-        }
+        });
     }
-})
+});
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
-})
+});
